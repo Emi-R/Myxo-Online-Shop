6be4f56a721470e27a34bbdd6c88ec5d54393168
Myxolidian <88684755+Emi-R@users.noreply.github.com>
2023-07-27 15:14:02 -0300

    First commit - Project Creation and DB Scripts

---
+++ Script Inserts.sql
@@ -0,0 +1,46 @@
+Use DBCARRITO
+Go
+
+Insert into USUARIO (Nombres, Apellidos, Correo, Clave) Values 
+	('TestNombre', 'TestApellido', 'test@example', 'ecd71870d1963316a97e3ac3408c9835ad8cf0f3c1bc703527c30265534f75ae')
+Go
+
+Insert into CATEGORIA(Descripcion) Values 
+	('Tecnologia'),
+	('Muebles'),
+	('Domitorio'),
+	('Deportes')
+Go
+
+Insert into MARCA(Descripcion) Values 
+	('Sony'),
+	('HP'),
+	('LG'),
+	('Hyundai'),
+	('Canon'),
+	('Roberta Allente')
+Go
+
+Insert into PROVINCIA(IdProvincia, Descripcion) values 
+	(1, 'Buenos Aires'),
+	(2, 'Córdoba'),
+	(3, 'Mendoza'),
+	(4, 'Santa Fe'),
+	(5, 'Tucumán'),
+	(6, 'Entre Ríos'),
+	(7, 'Salta'),
+	(8, 'Chaco'),
+	(9, 'Corrientes'),
+	(10, 'Misiones'),
+	(12, 'San Juan'),
+	(13, 'Jujuy'),
+	(14, 'Río Negro'),
+	(15, 'Neuquén'),
+	(16, 'Chubut')
+Go
+
+Insert into LOCALIDAD(IdLocalidad, Descripcion, IdProvincia) Values
+	(1, 'Palermo', 1),
+	(2, 'Belgrano', 1),
+	(3, 'San Fernando', 1)
+Go
---
+++ Script Tablas.sql
@@ -0,0 +1,103 @@
+Create Database DBCARRITO
+GO
+
+Use DBCARRITO
+GO
+
+Create Table CATEGORIA(
+	IdCategoria int primary key identity,
+	Descripcion varchar(100),
+	Activo bit default 1,
+	FechaRegistro datetime default getdate()
+)
+Go
+
+Create Table MARCA(
+	IdMarca int primary key identity,
+	Descripcion varchar(100),
+	Activo bit default 1,
+	FechaRegistro datetime default getdate()
+)
+Go
+
+Create Table PRODUCTO(
+	IdProducto int primary key identity,
+	Nombre varchar(100),
+	Descripcion varchar(100),
+	IdMarca int references MARCA(IdMarca),
+	IdCategoria int references CATEGORIA(IdCategoria),
+	Precio decimal (10,2) default 0,
+	Stock int,
+	RutaImagen varchar(100),
+	NombreImagen varchar(100),
+	Activo bit default 1,
+	FechaRegistro datetime default getdate()
+)
+Go
+
+Create Table CLIENTE(
+	IdCliente int primary key identity,
+	Nombres varchar(100),
+	Apellidos varchar(100),
+	Correo varchar(100),
+	Clave varchar(100),
+	Reestablecer bit default 0,
+	FechaRegistro datetime default getdate()
+)
+Go
+
+Create Table CARRITO(
+	IdCarrito int primary key identity,
+	IdCliente int references CLIENTE(IdCliente),
+	IdProducto int references PRODUCTO(IdProducto),
+	Cantidad int
+)
+Go
+
+Create Table VENTA(
+	IdVenta int primary key identity,
+	IdCliente int references CLIENTE(IdCliente),
+	TotalProducto int,
+	MontoTotal decimal(10,2),
+	Contacto varchar(50),
+	IdDistrito varchar(10),
+	Telefono varchar(50),
+	Direccion varchar(500),
+	IdTransaccion varchar(50),
+	FechaVenta datetime default getdate()
+)
+Go
+
+Create Table DETALLE_VENTA(
+	IdDetalleVenta int primary key identity,
+	IdVenta int references Venta(IDVenta),
+	IdProducto int references PRODUCTO(IdProducto),
+	Cantidad int,
+	Total decimal(10,2)
+)
+Go
+
+Create Table USUARIO(
+	IdUsuario int primary key identity,
+	Nombres varchar(100),
+	Apellidos varchar(100),
+	Correo varchar(100),
+	Clave varchar(100),
+	Reestablecer bit default 0,
+	Activo bit default 1,
+	FechaRegistro datetime default getdate()
+)
+Go
+
+Create Table LOCALIDAD (
+	IdLocalidad varchar(4) NOT NULL,
+	Descripcion varchar(45) NOT NULL,
+	IdProvincia varchar(2) NOT NULL
+)
+Go
+
+Create Table PROVINCIA (
+	IdProvincia varchar(4) NOT NULL,
+	Descripcion varchar(45) NOT NULL
+)
+Go
