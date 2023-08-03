388a190025b039d26860da5229d23e52be5a7319
Myxolidian <88684755+Emi-R@users.noreply.github.com>
2023-08-03 16:43:40 -0300

    add CRUD functions in Product business class - add create method on Controller

---
+++ Web App MVC/CapaNegocio/CN_Producto.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Producto
+    {
+        private CD_Producto objCapaDato = new CD_Producto();
+
+        public List<Producto> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        public int RegistrarProducto(Producto prod, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            //Text fields validations
+            if (string.IsNullOrEmpty(prod.Nombre) || string.IsNullOrWhiteSpace(prod.Nombre))
+            {
+                Mensaje = "El nombre no puede estar vacio";
+            }
+
+            else if (string.IsNullOrEmpty(prod.Descripcion) || string.IsNullOrWhiteSpace(prod.Descripcion))
+            {
+                Mensaje = "La descripcion no puede estar vacia";
+            }
+
+            else if (prod.oMarca.IdMarca == 0)
+            {
+                Mensaje = "Debe seleccionar una marca";
+            }
+
+            else if (prod.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Debe seleccionar una categoria";
+            }
+
+            else if (prod.Precio <= 0)
+            {
+                Mensaje = "Precio invalido";
+            }
+
+            else if (prod.Stock <= 0)
+            {
+                Mensaje = "Stock invalido";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+                return objCapaDato.RegistrarProducto(prod, out Mensaje);
+            else
+                return 0;
+        }
+
+        public bool EditarProducto(Producto prod, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            //Text fields validations
+            if (string.IsNullOrEmpty(prod.Nombre) || string.IsNullOrWhiteSpace(prod.Nombre))
+            {
+                Mensaje = "El nombre no puede estar vacio";
+            }
+
+            else if (string.IsNullOrEmpty(prod.Descripcion) || string.IsNullOrWhiteSpace(prod.Descripcion))
+            {
+                Mensaje = "La descripcion no puede estar vacia";
+            }
+
+            else if (prod.oMarca.IdMarca == 0)
+            {
+                Mensaje = "Debe seleccionar una marca";
+            }
+
+            else if (prod.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Debe seleccionar una categoria";
+            }
+
+            else if (prod.Precio <= 0)
+            {
+                Mensaje = "Precio invalido";
+            }
+
+            else if (prod.Stock <= 0)
+            {
+                Mensaje = "Stock invalido";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+                return objCapaDato.EditarProducto(prod, out Mensaje);
+            else
+                return false;
+        }
+
+        public bool EliminarProducto(int id, out string Mensaje)
+        {
+            return objCapaDato.EliminarProducto(id, out Mensaje);
+        }
+
+        public bool GuardarDatosImagen(Producto prod, out string Mensaje)
+        {
+            return objCapaDato.GuardarDatosImagen(prod, out Mensaje);
+        }
+    }
+}
--- Web App MVC/CapaPresentacionAdmin/Controllers/MantenedorController.cs
+++ Web App MVC/CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CapaEntidad;
 using CapaNegocio;
+using Newtonsoft.Json;
 
 namespace CapaPresentacionAdmin.Controllers
 {
@@ -94,6 +98,97 @@ namespace CapaPresentacionAdmin.Controllers
             }
 
             return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarMarca(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Marca().EliminarMarca(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+        #endregion
+
+        // -------------------------- Product ---------------------------
+        #region Product
+        [HttpGet]
+        public JsonResult ListarProductos()
+        {
+            List<Producto> oLista = new List<Producto>();
+            oLista = new CN_Producto().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarProducto(string prod, HttpPostedFileBase archivoImagen)
+        {
+            string mensaje = string.Empty;
+            bool success = true;
+            bool saveImageSucess = true;
+
+            Producto objProducto = new Producto();
+            objProducto = JsonConvert.DeserializeObject<Producto>(prod);
+
+            decimal precio;
+
+            if (decimal.TryParse(objProducto.PrecioTexto, NumberStyles.AllowDecimalPoint, new CultureInfo("es-AR"), out precio))
+                objProducto.Precio = precio;
+            else
+                return Json(success = false, mensaje = "El formato del precio debe ser ##.##", JsonRequestBehavior.AllowGet);
+
+
+            if (objProducto.IdProducto == 0)
+            {
+                int idProductoGenerado = new CN_Producto().RegistrarProducto(objProducto, out mensaje);
+
+                if(idProductoGenerado != 0)
+                    objProducto.IdProducto = idProductoGenerado;
+                else
+                    success = false;
+            }
+            else
+            {
+                success = new CN_Producto().EditarProducto(objProducto, out mensaje);
+            }
+
+            if(success)
+            {
+                if(archivoImagen != null)
+                {
+                    string path = ConfigurationManager.AppSettings["ServidorFotos"];
+                    string extension = Path.GetExtension(archivoImagen.FileName);
+                    string imageName = string.Concat(objProducto.IdProducto.ToString(), extension);
+
+                    try
+                    {
+                        archivoImagen.SaveAs(Path.Combine(path, imageName));
+                    }
+                    catch (Exception ex)
+                    {
+                        string msg = ex.Message;
+                        saveImageSucess = false;
+
+                    }
+
+                    if(saveImageSucess)
+                    {
+                        objProducto.RutaImagen = path;
+                        objProducto.NombreImagen = imageName;
+
+                        bool respuesta = new CN_Producto().GuardarDatosImagen(objProducto, out mensaje);
+                    }
+                    else
+                    {
+                        mensaje = "Se registro el producto pero hubo problemas con la imagen";
+                    }
+                }
+            }
+
+            return Json( new { operacionExitosa = success, idGenerado = objProducto.IdProducto, mensaje = mensaje, JsonRequestBehavior.AllowGet });
         }
 
         [HttpPost]
